59e03651e16d9db2cec248aa59bf753b86f97cff
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-29 13:49:43 -0700

    break out test into 2 tests

--- java401codechallenges/src/test/java/java401codechallenges/ArrayShiftTest.java
+++ java401codechallenges/src/test/java/java401codechallenges/ArrayShiftTest.java
@@ -5,15 +5,21 @@ import static org.junit.Assert.*;
 
 public class ArrayShiftTest {
     @Test
-    public void testRoll() {
+    public void testRollEvenArrayLength() {
         int[] inputArray1 = {1, 4};
+        int valueToInsert = 3;
+        int[] outputArray1 = {1, 3, 4};
+
+        assertArrayEquals("insertShiftArray method should return {{1, 3, 4}", outputArray1, ArrayShift.insertShiftArray(inputArray1, valueToInsert));
+    }
+
+    @Test
+    public void testRollOddArrayLength() {
         int[] inputArray2 = {1, 4, 2};
         int valueToInsert = 3;
 
-        int[] outputArray1 = {1, 3, 4};
         int[] outputArray2 = {1, 4, 3, 2};
 
-        assertArrayEquals("insertShiftArray method should return {{1, 3, 4}", outputArray1, ArrayShift.insertShiftArray(inputArray1, valueToInsert));
         assertArrayEquals("insertShiftArray method should return {{1, 4, 3, 2}", outputArray2, ArrayShift.insertShiftArray(inputArray2, valueToInsert));
 
     }
