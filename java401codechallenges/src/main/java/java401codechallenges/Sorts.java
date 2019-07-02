ad482e81c0de11b5a4715cc2c498ec822d68d17b
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-07-02 14:57:04 -0700

    add tests

---
+++ java401codechallenges/src/main/java/java401codechallenges/Sorts.java
@@ -0,0 +1,20 @@
+package java401codechallenges;
+
+public class Sorts {
+
+    public static int[] insertionSort(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            int j = i - 1;
+            int temp = arr[i];
+
+            while(j >= 0 && temp < arr[j]){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = temp;
+        }
+
+        return arr;
+    }
+}
